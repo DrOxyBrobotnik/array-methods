cac80d6e45b04a0fa05a2ec08290b261deb10a72
Katt <Raxxyn@gmail.com>
2022-12-18 21:32:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+/* static int BigMeow(int[] array)
+{
+    var smallest = array[0];
+    foreach (var element in array)
+    {
+        if (element < smallest)
+            smallest = element;
+    }
+
+    return smallest; 
+}*/
+
+
+static int BigMeow(int[], )
+{
+    int x = 7;
+    var largest = array[0];
+    foreach (var element in array)
+    {
+        if (element == x)
+            largest = element;
+        
+    }
+
+    return largest;
+}
+
+static void Main(string[] args)
+{
+    var array = new[]{ 0, 5, 7, 6, 3 };
+    var result = BigMeow(array);
+
+    Console.Write(result);
+}
+
+
+
+
